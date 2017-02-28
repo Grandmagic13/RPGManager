d3e5f360f36aae796f5bed73e746a328281f1331
Grandmagic13 <jabencefear@hotmail.com>
2017-02-28 23:06:27 +0100

    Created new parameterized test and moved resources to common package
    - Created parameterized test class for testing invalid background and
    base class pairings
    - moved backgrounds sorted by base class compatibility under a new
    resources package to BackgroundUnitTestData.java. This class might be
    changed to a simple resource file (JSON, XML, etc) in the future

--- RPGManager_development/test/unit_test/character_sheet_unit_tests/CharacterSheetInvalidBackgroundUnitTests.java
+++ RPGManager_development/test/unit_test/character_sheet_unit_tests/CharacterSheetInvalidBackgroundUnitTests.java
@@ -3,6 +3,7 @@ package unit_test.character_sheet_unit_tests;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 import org.junit.Test;
@@ -15,44 +16,40 @@ import rpg_database.character_sheet.Background;
 import rpg_database.character_sheet.CharacterSheet;
 import rpg_database.character_sheet.Fields;
 import rpg_database.character_sheet.character_class.BaseClasses;
+import rpg_database.character_sheet.character_class.InvalidCharacterClassException;
 import unit_test.character_sheet_unit_tests.resources.BackgroundUnitTestData;
 
 @RunWith(Parameterized.class)
-public class CharacterSheetValidBackgroundUnitTests {
+public class CharacterSheetInvalidBackgroundUnitTests {
 
 	// Description:
 	//
-	// This class tests setting up various valid backgrounds to their base
+	// This class tests setting up various invalid backgrounds to their base
 	// classes (mage, rogue, warrior)
 	//
-	// Every valid class - background pairing is tested
+	// Every invalid class - background pairing is tested
 
 	@Parameters(name = "{index}: Class: ''{1}'' Background: ''{0}''")
 	public static Collection<Object[]> data() {
 		ArrayList<Object[]> parameters = new ArrayList<>();
+
 		for (BaseClasses baseClass : BaseClasses.values()) {
 			if (baseClass.equals(BaseClasses.MAGE)) {
-				for (Background background : BackgroundUnitTestData.mageOnlyBackgrounds) {
-					parameters.add(new Object[] { background, baseClass });
-				}
-				for (Background background : BackgroundUnitTestData.allClassesBackgrounds) {
-					parameters.add(new Object[] { background, baseClass });
-				}
-			} else if (baseClass.equals(BaseClasses.ROGUE)) {
 				for (Background background : BackgroundUnitTestData.rogueOnlyBackgrounds) {
 					parameters.add(new Object[] { background, baseClass });
 				}
 				for (Background background : BackgroundUnitTestData.rogueAndWarriorBackgrounds) {
 					parameters.add(new Object[] { background, baseClass });
 				}
-				for (Background background : BackgroundUnitTestData.allClassesBackgrounds) {
+			} else if (baseClass.equals(BaseClasses.ROGUE)) {
+				for (Background background : BackgroundUnitTestData.mageOnlyBackgrounds) {
 					parameters.add(new Object[] { background, baseClass });
 				}
 			} else if (baseClass.equals(BaseClasses.WARRIOR)) {
-				for (Background background : BackgroundUnitTestData.rogueAndWarriorBackgrounds) {
+				for (Background background : BackgroundUnitTestData.mageOnlyBackgrounds) {
 					parameters.add(new Object[] { background, baseClass });
 				}
-				for (Background background : BackgroundUnitTestData.allClassesBackgrounds) {
+				for (Background background : BackgroundUnitTestData.rogueOnlyBackgrounds) {
 					parameters.add(new Object[] { background, baseClass });
 				}
 			}
@@ -66,12 +63,10 @@ public class CharacterSheetValidBackgroundUnitTests {
 	@Parameter(1)
 	public BaseClasses baseClass;
 
-	@Test
-	public void testSetBackground() {
+	@Test(expected = InvalidCharacterClassException.class)
+	public void testSetInvalidBackground() {
 		CharacterSheet testCharacterSheet = new CharacterSheet("TestCharacterSheet");
 		testCharacterSheet.setData(Fields.BASECLASS, baseClass);
 		testCharacterSheet.setData(Fields.BACKGROUND, background);
-		Background background = testCharacterSheet.getData(Fields.BACKGROUND);
-		assertEquals(this.background, background);
 	}
 }
---
+++ RPGManager_development/test/unit_test/character_sheet_unit_tests/resources/BackgroundUnitTestData.java
@@ -0,0 +1,21 @@
+package unit_test.character_sheet_unit_tests.resources;
+
+import rpg_database.character_sheet.Background;
+
+final public class BackgroundUnitTestData {
+	final static public Background[] mageOnlyBackgrounds = { Background.APOSTATE, Background.CIRCLE_MAGE, Background.TEVINTER_ALTUS,
+			Background.TEVINTER_LAETAN };
+	final static public Background[] rogueOnlyBackgrounds = { Background.DWARF_DUSTER };
+	final static public Background[] rogueAndWarriorBackgrounds = { Background.ANTIVAN_WAYFARER, Background.CITY_ELF, Background.FERELDAN_CRAFTSMEN,
+			Background.FERELDAN_FREEMAN, Background.FERELDAN_NOBLE, Background.FREE_MARCHER, Background.HIGH_BORN_DWARF, Background.LOW_BORN_DWARF,
+			Background.NEVARRAN_ADVENTURER, Background.ORLESIAN_COMMONER, Background.ORLESIAN_NOBLE, Background.ORLESIAN_STUDENT,
+			Background.QUNARI_BERESAAD, Background.RIVAINI_MERCHANT, Background.SEHERON_CONVERT, Background.SURFACE_DWARF,
+			Background.TEVINTER_SOPORATI, Background.WAKING_SEA_RAIDER };
+	final static public Background[] allClassesBackgrounds = { Background.ANDER_SURVIVOR, Background.AVVAR, Background.CHASIND_WILDER,
+			Background.DALISH_ELF, Background.ESCAPED_ELVEN_SLAVE, Background.ORLESIAN_EXILE, Background.TAL_VASHOTH };
+
+	private BackgroundUnitTestData() {
+
+	}
+
+}
