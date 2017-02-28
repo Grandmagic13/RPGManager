bf59eff222fdafc5961e7990c20c135a285d9c8f
Grandmagic13 <jabencefear@hotmail.com>
2017-02-28 23:38:32 +0100

    Created positive and negative parameterized tests for Specialization and Base class pairings
    - created resource for testing -> still incomplete, needs expanding

---
+++ RPGManager_development/test/unit_test/character_sheet_unit_tests/resources/SpecializationCompatibilityData.java
@@ -0,0 +1,9 @@
+package unit_test.character_sheet_unit_tests.resources;
+
+import rpg_database.character_sheet.character_class.SpecializationClasses;
+
+public class SpecializationCompatibilityData {
+	final static public SpecializationClasses[] mageSpecializations = { SpecializationClasses.ARCANE_WARRIOR };
+	final static public SpecializationClasses[] rogueSpecializations = { SpecializationClasses.ASSASSIN };
+	final static public SpecializationClasses[] warriorSpecializations = { SpecializationClasses.BERSERKER };
+}
