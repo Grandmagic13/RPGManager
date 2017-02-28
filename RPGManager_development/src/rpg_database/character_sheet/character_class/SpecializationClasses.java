cbc3998fc5cb76fbaadf6b86cf876065f1a9cdbc
Grandmagic13 <jabencefear@hotmail.com>
2017-03-01 00:03:28 +0100

    Added all known specialization classes to production and test

--- RPGManager_development/src/rpg_database/character_sheet/character_class/SpecializationClasses.java
+++ RPGManager_development/src/rpg_database/character_sheet/character_class/SpecializationClasses.java
@@ -3,7 +3,14 @@ package rpg_database.character_sheet.character_class;
 public enum SpecializationClasses {
 	// TODO specializationClasses!
 	NOT_APPLICABLE("N/A"), ARCANE_WARRIOR("Arcane Warrior", BaseClasses.MAGE), ASSASSIN("Assassin", BaseClasses.ROGUE),
-	BERSERKER("Berserker", BaseClasses.WARRIOR), CHAMPION("Champion", BaseClasses.WARRIOR);
+	BARD("Bard", BaseClasses.ROGUE), BERSERKER("Berserker", BaseClasses.WARRIOR), BLOOD_MAGE("Blood Mage", BaseClasses.MAGE),
+	CHAMPION("Champion", BaseClasses.WARRIOR), CHEVALIER("Chevalier", BaseClasses.WARRIOR), DUELIST("Duelist", BaseClasses.ROGUE),
+	FORCE_MAGE("Force Mage", BaseClasses.MAGE), GUARDIAN("Guardian", BaseClasses.WARRIOR), KEEPER("Keeper", BaseClasses.MAGE),
+	LEGIONNAIRE_SCOUT("Legionnaire Scout", BaseClasses.ROGUE), LEGIONNAIRE_WARRIOR("Legionnaire Warrior", BaseClasses.WARRIOR),
+	LYRIUM_WARRIOR("Lyrium Warrior", BaseClasses.WARRIOR), NECROMANCER("Necromancer", BaseClasses.MAGE), MARKSMAN("Marksman", BaseClasses.ROGUE),
+	RANGER("Ranger", BaseClasses.ROGUE), REAVER("Reaver", BaseClasses.WARRIOR), SAAREBAS("Saarebas", BaseClasses.MAGE),
+	SHADOW("Shadow", BaseClasses.ROGUE), SHAPESHIFTER("Shapeshifter", BaseClasses.MAGE), SPIRIT_HEALER("Spirit Healer", BaseClasses.MAGE),
+	SPIRIT_WARRIOR("Spirit Warrior", BaseClasses.WARRIOR), TEMPEST("Tempest", BaseClasses.ROGUE), TEMPLAR("Templar", BaseClasses.WARRIOR);
 
 	private final String text;
 	private final BaseClasses baseClass;
