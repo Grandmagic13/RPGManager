648a8f805e63a147a8d4639feff7ef2a10d01132
Pluci <plutzer.mate@gmail.com>
2017-03-03 13:27:21 +0100

    Created new simple int Fields like Defense, Armor, Health, Mana, Level

--- RPGManager_development/src/rpg_database/character_sheet/Fields.java
+++ RPGManager_development/src/rpg_database/character_sheet/Fields.java
@@ -2,7 +2,8 @@ package rpg_database.character_sheet;
 
 public enum Fields {
 	NAME(String.class), AGE(Integer.class), XP(Integer.class), GENDER(Gender.class), SPEED(Integer.class), BACKGROUND(Background.class),
-	BASECLASS(BaseClasses.class), SPECIALIZATIONCLASS(SpecializationClasses.class), MONEY(Integer.class);
+	BASECLASS(BaseClasses.class), SPECIALIZATIONCLASS(SpecializationClasses.class), MONEY(Integer.class), LEVEL(Integer.class),
+	DEFENSE(Integer.class), ARMOR(Integer.class), HEALTH(Integer.class), MANA(Integer.class);
 
 	private Class<?> fieldClass;
 
