8fc2e3bdb4aaadf155481844dbc7d0579e32861b
Grandmagic13 <jabencefear@hotmail.com>
2017-03-06 21:17:12 +0100

    Moved BaseClass dependent unit tests into 3 seperate classes for:
    - BaseClass
    - SpecializationClass
    - Background

---
+++ RPGManager_development/test/unit_test/character_sheet_unit_tests/BaseClassUnitTests.java
@@ -0,0 +1,60 @@
+package unit_test.character_sheet_unit_tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import rpg_database.character_sheet.BaseClasses;
+import rpg_database.character_sheet.CharacterSheet;
+import rpg_database.character_sheet.Fields;
+import rpg_database.character_sheet.SpecializationClasses;
+import rpg_database.character_sheet.exceptions.InvalidCharacterClassException;
+
+import static unit_test.character_sheet_unit_tests.common.CommonMethods.*;
+
+public class BaseClassUnitTests {
+
+	// test methods
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void testSetCharacterBaseClassMage() {
+		CharacterSheet characterSheet = new CharacterSheet("CharacterSheet");
+		characterSheet.setData(Fields.BASECLASS, BaseClasses.MAGE);
+		BaseClasses actualClass = characterSheet.getData(Fields.BASECLASS);
+		assertEquals(BaseClasses.MAGE, actualClass);
+	}
+
+	@Test
+	public void testSetCharacterBaseClassRogue() {
+		CharacterSheet characterSheet = new CharacterSheet("CharacterSheet");
+		characterSheet.setData(Fields.BASECLASS, BaseClasses.ROGUE);
+		BaseClasses actualClass = characterSheet.getData(Fields.BASECLASS);
+		assertEquals(BaseClasses.ROGUE, actualClass);
+	}
+
+	@Test
+	public void expectException_SetCharacterBaseClassFromMageToWarrior_ArcaneWarrior() {
+		expectExceptionWithMessage(InvalidCharacterClassException.class, "Warrior is not a base class of Arcane Warrior");
+		CharacterSheet characterSheet = createCharacterSheetWithCustomClasses(BaseClasses.MAGE, SpecializationClasses.ARCANE_WARRIOR);
+		characterSheet.setData(Fields.BASECLASS, BaseClasses.WARRIOR);
+	}
+
+	@Test
+	public void expectException_SetCharacterBaseClassFromWarriorToMage_Berserker() {
+		expectExceptionWithMessage(InvalidCharacterClassException.class, "Mage is not a base class of Berserker");
+		CharacterSheet characterSheet = createCharacterSheetWithCustomClasses(BaseClasses.WARRIOR, SpecializationClasses.BERSERKER);
+		characterSheet.setData(Fields.BASECLASS, BaseClasses.MAGE);
+	}
+
+	// private methods
+
+	private void expectExceptionWithMessage(Class<? extends Exception> exceptionClass, String message) {
+		thrown.expect(exceptionClass);
+		thrown.expectMessage(message);
+	}
+
+}
--- RPGManager_development/test/unit_test/test_suits/CharacterSheetTestSuite.java
+++ RPGManager_development/test/unit_test/test_suits/CharacterSheetTestSuite.java
@@ -3,18 +3,20 @@ package unit_test.test_suits;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import unit_test.character_sheet_unit_tests.BaseClassDependentUnitTests;
+import unit_test.character_sheet_unit_tests.BackgroundUnitTests;
+import unit_test.character_sheet_unit_tests.BaseClassUnitTests;
 import unit_test.character_sheet_unit_tests.CharacterSheetUnitTests;
 import unit_test.character_sheet_unit_tests.GetDefaultValuesUnitTest;
 import unit_test.character_sheet_unit_tests.InvalidBackgroundUnitTests;
 import unit_test.character_sheet_unit_tests.InvalidClassAndSpecializationPairingsTests;
 import unit_test.character_sheet_unit_tests.MoneyUnitTests;
+import unit_test.character_sheet_unit_tests.SpecializationClassUnitTests;
 import unit_test.character_sheet_unit_tests.ValidBackgroundUnitTests;
 import unit_test.character_sheet_unit_tests.ValidClassAndSpecializationPairingsTests;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ GetDefaultValuesUnitTest.class, CharacterSheetUnitTests.class, BaseClassDependentUnitTests.class,
-		InvalidBackgroundUnitTests.class, InvalidClassAndSpecializationPairingsTests.class, ValidBackgroundUnitTests.class,
+@Suite.SuiteClasses({ GetDefaultValuesUnitTest.class, CharacterSheetUnitTests.class, BaseClassUnitTests.class, SpecializationClassUnitTests.class,
+		BackgroundUnitTests.class, InvalidBackgroundUnitTests.class, InvalidClassAndSpecializationPairingsTests.class, ValidBackgroundUnitTests.class,
 		ValidClassAndSpecializationPairingsTests.class, MoneyUnitTests.class })
 
 public class CharacterSheetTestSuite {
