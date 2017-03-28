168866116ed91b7bb32a2b6521260bbed1c45e80
Grandmagic13 <jabencefear@hotmail.com>
2017-03-28 20:31:13 +0200

    Split Circle mage background into human circle mage and elf circle mage

--- RPGManager_development/src/rpg_database/character_sheet/Background.java
+++ RPGManager_development/src/rpg_database/character_sheet/Background.java
@@ -10,19 +10,20 @@ public enum Background implements CustomSetter<Background> {
 	// TODO lot of enum information. Factory class for reading from file?
 	ANDER_SURVIVOR("Ander Survivor", playAs_Mage_Warrior_Rogue()), ANTIVAN_WAYFARER("Antivan Wayfarer", playAs_Warrior_Rogue()),
 	AVVAR("Avvar", playAs_Mage_Warrior_Rogue()), CHASIND_WILDER("Chasind Wilder", playAs_Mage_Warrior_Rogue()),
-	CIRCLE_MAGE("Circle Mage", playAs_Mage()), CITY_ELF("City Elf", playAs_Warrior_Rogue()), DALISH_ELF("Dalish Elf", playAs_Mage_Warrior_Rogue()),
-	DWARF_DUSTER("Dwarf Duster", playAs_Rogue()), ELF_APOSTATE("Elf Apostate", playAs_Mage()),
+	CITY_ELF("City Elf", playAs_Warrior_Rogue()), DALISH_ELF("Dalish Elf", playAs_Mage_Warrior_Rogue()), DWARF_DUSTER("Dwarf Duster", playAs_Rogue()),
+	ELF_APOSTATE("Elf Apostate", playAs_Mage()), ELF_CIRCLE_MAGE("Circle Mage", playAs_Mage()),
 	ESCAPED_ELVEN_SLAVE("Escaped Elven Slave", playAs_Mage_Warrior_Rogue()), FERELDAN_CRAFTSMEN("Fereldan Craftsmen", playAs_Warrior_Rogue()),
 	FERELDAN_FREEMAN("Fereldan Freeman", playAs_Warrior_Rogue()), FERELDAN_NOBLE("Fereldan Noble", playAs_Warrior_Rogue()),
 	FREE_MARCHER("Free Marcher", playAs_Warrior_Rogue()), HIGH_BORN_DWARF("High Born Dwarf", playAs_Warrior_Rogue()),
-	HUMAN_APOSTATE("Human Apostate", playAs_Mage()), LOW_BORN_DWARF("Low Born Dwarf", playAs_Warrior_Rogue()),
-	NEVARRAN_ADVENTURER("Nevarran Adventurer", playAs_Warrior_Rogue()), ORLESIAN_COMMONER("Orlesian Commoner", playAs_Warrior_Rogue()),
-	ORLESIAN_EXILE("Orlesian Exile", playAs_Mage_Warrior_Rogue()), ORLESIAN_NOBLE("Orlesian Noble", playAs_Warrior_Rogue()),
-	ORLESIAN_STUDENT("Orlesian Student", playAs_Warrior_Rogue()), QUNARI_BERESAAD("Qunari Beresaad", playAs_Warrior_Rogue()),
-	RIVAINI_MERCHANT("Rivaini Merchant", playAs_Warrior_Rogue()), SEHERON_CONVERT("Seheron Convert", playAs_Warrior_Rogue()),
-	SURFACE_DWARF("Surface Dwarf", playAs_Warrior_Rogue()), TAL_VASHOTH("Tal-Vashoth", playAs_Mage_Warrior_Rogue()),
-	TEVINTER_ALTUS("Tevinter Altus", playAs_Mage()), TEVINTER_LAETAN("Tevinter Laetan", playAs_Mage()),
-	TEVINTER_SOPORATI("Tevinter Soporati", playAs_Warrior_Rogue()), WAKING_SEA_RAIDER("Waking Sea Raider", playAs_Warrior_Rogue());
+	HUMAN_APOSTATE("Human Apostate", playAs_Mage()), HUMAN_CIRCLE_MAGE("Circle Mage", playAs_Mage()),
+	LOW_BORN_DWARF("Low Born Dwarf", playAs_Warrior_Rogue()), NEVARRAN_ADVENTURER("Nevarran Adventurer", playAs_Warrior_Rogue()),
+	ORLESIAN_COMMONER("Orlesian Commoner", playAs_Warrior_Rogue()), ORLESIAN_EXILE("Orlesian Exile", playAs_Mage_Warrior_Rogue()),
+	ORLESIAN_NOBLE("Orlesian Noble", playAs_Warrior_Rogue()), ORLESIAN_STUDENT("Orlesian Student", playAs_Warrior_Rogue()),
+	QUNARI_BERESAAD("Qunari Beresaad", playAs_Warrior_Rogue()), RIVAINI_MERCHANT("Rivaini Merchant", playAs_Warrior_Rogue()),
+	SEHERON_CONVERT("Seheron Convert", playAs_Warrior_Rogue()), SURFACE_DWARF("Surface Dwarf", playAs_Warrior_Rogue()),
+	TAL_VASHOTH("Tal-Vashoth", playAs_Mage_Warrior_Rogue()), TEVINTER_ALTUS("Tevinter Altus", playAs_Mage()),
+	TEVINTER_LAETAN("Tevinter Laetan", playAs_Mage()), TEVINTER_SOPORATI("Tevinter Soporati", playAs_Warrior_Rogue()),
+	WAKING_SEA_RAIDER("Waking Sea Raider", playAs_Warrior_Rogue());
 
 	private final String text;
 	private final ArrayList<BaseClasses> baseClasses;
