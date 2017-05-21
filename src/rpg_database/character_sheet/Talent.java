7f3a0fb9a72e4a85f89c7397cc2d932f89b7e079
Grandmagic13 <jabencefear@hotmail.com>
2017-05-21 15:06:57 +0200

    Implemented talent level setter, talent enum better, and first rule entry of allowed base classes

--- src/rpg_database/character_sheet/Talent.java
+++ src/rpg_database/character_sheet/Talent.java
@@ -1,18 +1,41 @@
 package rpg_database.character_sheet;
 
+import java.util.ArrayList;
+
+import rpg_database.character_sheet.common.FieldRules;
+import rpg_database.character_sheet.common.FieldRulesFactory;
+import rpg_database.character_sheet.common.Keys;
+
 public class Talent {
 
-	private final TalentLevels talentLevel;
+	private TalentLevels talentLevel;
+	private final Talents talentName;
+	private final ArrayList<BaseClasses> allowedBaseClasses;
 
-	public Talent() {
-		this(TalentLevels.NOVICE);
+	public Talent(Talents talentName) {
+		this(talentName, TalentLevels.NOVICE);
 	}
 
-	public Talent(TalentLevels talentLevel) {
+	public Talent(Talents talentName, TalentLevels talentLevel) {
 		this.talentLevel = talentLevel;
+		this.talentName = talentName;
+		FieldRules talentsRule = FieldRulesFactory.getFieldRules(FieldRulesFactory.TALENTS);
+		this.allowedBaseClasses = talentsRule.getEnumsForField(talentName, BaseClasses.class, Keys.BASE_CLASSES_ARRAY);
 	}
 
 	public TalentLevels getTalentLevel() {
 		return talentLevel;
+	}
+
+	public void setTalentLevel(TalentLevels talentLevel) {
+		this.talentLevel = talentLevel;
+	}
+
+	public Talents getTalentName() {
+		return talentName;
+	}
+
+	public ArrayList<BaseClasses> getAllowedBaseClasses() {
+		return allowedBaseClasses;
 	}
 }
--- test/unit_test/character_sheet_unit_tests/TalentTests.java
+++ test/unit_test/character_sheet_unit_tests/TalentTests.java
@@ -1,13 +1,18 @@
 package unit_test.character_sheet_unit_tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import rpg_database.character_sheet.BaseClasses;
 import rpg_database.character_sheet.Talent;
 import rpg_database.character_sheet.TalentLevels;
+import rpg_database.character_sheet.Talents;
 
 public class TalentTests {
 
@@ -17,16 +22,48 @@ public class TalentTests {
 
 	// test methods
 	@Test
-	public void testDefaultTalentLevel() {
-		Talent talent = new Talent();
+	public void testGetDefaultTalentLevel() {
+		Talent talent = new Talent(Talents.ALCHEMY);
 		assertEquals(TalentLevels.NOVICE, talent.getTalentLevel());
 	}
 
 	@Test
-	public void testMasterTalentLevel() {
-		Talent talent = new Talent(TalentLevels.MASTER);
+	public void testGetMasterTalentLevel() {
+		Talent talent = new Talent(Talents.ALCHEMY, TalentLevels.MASTER);
 		assertEquals(TalentLevels.MASTER, talent.getTalentLevel());
 	}
+
+	@Test
+	public void testSetMasterTalentLevel() {
+		Talent talent = new Talent(Talents.ALCHEMY, TalentLevels.MASTER);
+		talent.setTalentLevel(TalentLevels.JOURNEYMAN);
+		assertEquals(TalentLevels.JOURNEYMAN, talent.getTalentLevel());
+	}
+
+	@Test
+	public void testGetTalentName() {
+		Talent talent = new Talent(Talents.ALCHEMY, TalentLevels.MASTER);
+		assertEquals(Talents.ALCHEMY, talent.getTalentName());
+	}
+
+	@Test
+	public void testGetAllowedBaseClassesAlchemy() {
+		Talent talent = new Talent(Talents.ALCHEMY);
+		ArrayList<BaseClasses> expectedAllowedMageRogueWarrior = new ArrayList<>(Arrays.asList(BaseClasses.MAGE, BaseClasses.ROGUE,
+				BaseClasses.WARRIOR));
+		assertEquals(expectedAllowedMageRogueWarrior, talent.getAllowedBaseClasses());
+	}
+
+	@Test
+	public void testGetAllowedBaseClassesPoleWeaponStyle() {
+		Talent talent = new Talent(Talents.POLE_WEAPON_STYLE);
+		ArrayList<BaseClasses> expectedAllowedBaseClassWarrior = new ArrayList<>(Arrays.asList(BaseClasses.WARRIOR));
+		assertEquals(expectedAllowedBaseClassWarrior, talent.getAllowedBaseClasses());
+	}
+
+	// requirements
+
+	// descriptions
 
 	// private methods
 
